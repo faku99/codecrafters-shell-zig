9e150b6fd614fae85affa532ce4e05d9ae893671
Lucas Elisei <lelisei@tandemdiabetes.com>
2025-01-24 17:24:18 +0100

    REPL

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,19 @@
 const std = @import("std");
 
+fn handleCommand(input: []const u8) void {
+    std.debug.print("{s}: command not found\n", .{input});
+}
+
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-    try stdout.print("$ ", .{});
+    while (true) {
+        const stdout = std.io.getStdOut().writer();
+        try stdout.print("$ ", .{});
 
-    const stdin = std.io.getStdIn().reader();
-    var buffer: [1024]u8 = undefined;
-    const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+        const stdin = std.io.getStdIn().reader();
+        var buffer: [1024]u8 = undefined;
 
-    std.debug.print("{s}: command not found\n", .{user_input});
+        const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+
+        handleCommand(user_input);
+    }
 }
