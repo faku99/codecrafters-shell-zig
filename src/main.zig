1784e0bea4a6b2f5e817d2758f50bda480ccc82c
Lucas Elisei <lelisei@tandemdiabetes.com>
2025-01-24 18:06:38 +0100

    The exit builtin

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,42 @@
 const std = @import("std");
 
-fn handleCommand(input: []const u8) void {
-    std.debug.print("{s}: command not found\n", .{input});
+const Command = enum {
+    exit,
+    unknown,
+};
+
+const ParsedInput = struct {
+    command: Command,
+    arguments: [][]const u8,
+};
+
+fn handleCommand(input: ParsedInput) void {
+    switch (input.command) {
+        .exit => std.process.exit(std.fmt.parseUnsigned(u8, input.arguments[1], 10) catch 1),
+        else => std.debug.print("{s}: command not found\n", .{input.arguments[0]}),
+    }
+}
+
+fn parseInput(allocator: std.mem.Allocator, input: []const u8) !ParsedInput {
+    var tokens = std.ArrayList([]const u8).init(allocator);
+    defer tokens.deinit();
+
+    var it = std.mem.tokenizeAny(u8, input, " ");
+    while (it.next()) |token| {
+        try tokens.append(token);
+    }
+
+    const parsed_input = ParsedInput{
+        .command = std.meta.stringToEnum(Command, tokens.items[0]) orelse .unknown,
+        .arguments = try tokens.toOwnedSlice(),
+    };
+
+    return parsed_input;
 }
 
 pub fn main() !void {
+    const allocator = std.heap.page_allocator;
+
     while (true) {
         const stdout = std.io.getStdOut().writer();
         try stdout.print("$ ", .{});
@@ -13,7 +45,8 @@ pub fn main() !void {
         var buffer: [1024]u8 = undefined;
 
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
+        const command = try parseInput(allocator, user_input);
 
-        handleCommand(user_input);
+        handleCommand(command);
     }
 }
