7856946bccf78db99061e12d464f2de24f18199a
Lucas Elisei <lelisei@tandemdiabetes.com>
2025-01-24 22:54:30 +0100

    Run a program

--- src/main.zig
+++ src/main.zig
@@ -2,19 +2,33 @@ const std = @import("std");
 
 const ALLOCATOR = std.heap.page_allocator;
 
-const Command = enum {
+const CommandError = error{
+    NotFound,
+};
+
+const Builtin = enum {
     echo,
     exit,
     type,
-    unknown,
 };
 
-const ParsedInput = struct {
-    command: Command,
+const CommandType = enum {
+    builtin,
+    executable,
+};
+
+const CommandExe = union {
+    builtin: Builtin,
+    path: []const u8,
+};
+
+const Command = struct {
+    type: CommandType,
     arguments: [][]const u8,
+    exe: CommandExe,
 };
 
-fn echoCmd(input: ParsedInput) void {
+fn echoCmd(input: Command) void {
     for (1..input.arguments.len) |i| {
         if (i > 1) std.debug.print(" ", .{});
         std.debug.print("{s}", .{input.arguments[i]});
@@ -44,20 +58,19 @@ fn findInPath(allocator: std.mem.Allocator, file: []const u8) !?[]const u8 {
     return null;
 }
 
-fn typeCmd(allocator: std.mem.Allocator, input: ParsedInput) !void {
-    if (std.meta.stringToEnum(Command, input.arguments[1]) != null) {
-        std.debug.print("{s} is a shell builtin\n", .{input.arguments[1]});
+fn typeCmd(command: Command) void {
+    const target = parseInput(ALLOCATOR, command.arguments[1]) catch {
+        std.debug.print("{s}: not found\n", .{command.arguments[1]});
         return;
-    }
+    };
 
-    if (try findInPath(allocator, input.arguments[1])) |path| {
-        std.debug.print("{s} is {s}/{s}\n", .{ input.arguments[1], path, input.arguments[1] });
-    } else {
-        std.debug.print("{s}: not found\n", .{input.arguments[1]});
+    switch (target.type) {
+        .builtin => std.debug.print("{s} is a shell builtin\n", .{target.arguments[0]}),
+        .executable => std.debug.print("{s} is {s}\n", .{ target.arguments[0], target.exe.path }),
     }
 }
 
-fn parseInput(allocator: std.mem.Allocator, input: []const u8) !ParsedInput {
+fn parseInput(allocator: std.mem.Allocator, input: []const u8) !Command {
     var tokens = std.ArrayList([]const u8).init(allocator);
     defer tokens.deinit();
 
@@ -66,12 +79,69 @@ fn parseInput(allocator: std.mem.Allocator, input: []const u8) !ParsedInput {
         try tokens.append(token);
     }
 
-    const parsed_input = ParsedInput{
-        .command = std.meta.stringToEnum(Command, tokens.items[0]) orelse .unknown,
-        .arguments = try tokens.toOwnedSlice(),
+    // Check if builtin
+    if (std.meta.stringToEnum(Builtin, tokens.items[0])) |builtin| {
+        return Command{
+            .type = .builtin,
+            .arguments = try tokens.toOwnedSlice(),
+            .exe = CommandExe{ .builtin = builtin },
+        };
+    }
+
+    // Check if executable
+    if (try findInPath(allocator, tokens.items[0])) |directory| {
+        var path = std.ArrayList(u8).init(allocator);
+        defer path.deinit();
+
+        try path.appendSlice(directory);
+        try path.appendSlice("/");
+        try path.appendSlice(tokens.items[0]);
+
+        return Command{
+            .type = .executable,
+            .arguments = try tokens.toOwnedSlice(),
+            .exe = CommandExe{
+                .path = try path.toOwnedSlice(),
+            },
+        };
+    }
+
+    return CommandError.NotFound;
+}
+
+fn handleExecutable(command: Command) void {
+    var child = std.process.Child.init(command.arguments, ALLOCATOR);
+    child.stdout_behavior = .Pipe;
+
+    child.spawn() catch {
+        std.log.err("Error executing '{s}'\n", .{command.arguments[0]});
+        return;
     };
 
-    return parsed_input;
+    if (child.stdout) |stdout| {
+        std.io.getStdOut().writeFileAll(stdout, .{}) catch |e| {
+            std.log.err("Error: {s}\n", .{@typeName(@TypeOf(e))});
+        };
+    }
+
+    _ = child.wait() catch |e| {
+        std.log.err("Error: {s}\n", .{@typeName(@TypeOf(e))});
+    };
+}
+
+fn handleBuiltin(command: Command) void {
+    switch (command.exe.builtin) {
+        .echo => echoCmd(command),
+        .exit => std.process.exit(std.fmt.parseUnsigned(u8, command.arguments[1], 10) catch 1),
+        .type => typeCmd(command),
+    }
+}
+
+fn handleCommand(command: Command) void {
+    switch (command.type) {
+        .builtin => handleBuiltin(command),
+        .executable => handleExecutable(command),
+    }
 }
 
 pub fn main() !void {
@@ -83,13 +153,18 @@ pub fn main() !void {
         var buffer: [1024]u8 = undefined;
 
         const user_input = try stdin.readUntilDelimiter(&buffer, '\n');
-        const input = try parseInput(ALLOCATOR, user_input);
 
-        switch (input.command) {
-            .echo => echoCmd(input),
-            .exit => std.process.exit(std.fmt.parseUnsigned(u8, input.arguments[1], 10) catch 1),
-            .type => try typeCmd(ALLOCATOR, input),
-            .unknown => std.debug.print("{s}: command not found\n", .{input.arguments[0]}),
-        }
+        const input = parseInput(ALLOCATOR, user_input) catch |e| switch (e) {
+            CommandError.NotFound => {
+                std.debug.print("{s}: not found\n", .{user_input});
+                continue;
+            },
+            else => {
+                std.log.err("error: {}\n", .{e});
+                std.process.exit(1);
+            },
+        };
+
+        handleCommand(input);
     }
 }
